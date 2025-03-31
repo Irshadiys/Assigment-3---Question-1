313889fa829508c35932248d092a8a3ac531528d
Irshad <K00297724@student.tus.ie>
2025-03-31 21:25:02 +0100

    full code with comments

--- Assigment 3 - Question 1/Form1.cs
+++ Assigment 3 - Question 1/Form1.cs
@@ -3,38 +3,38 @@ namespace Assigment_3___Question_1
     public partial class Form1 : Form
     {
         public string[] arr = { "Jon", "Dot", "Eva", "Roy", "Guy", "Jan", "Tom", "Jim", "Ann", "Kim", "Ron", "Tim", "Kay", "Amy" };
-        public void HeapSort(string[] arr)
+        public void HeapSort(string[] arr) // we are taken our string array first
         {
             int lenth = arr.Length;
 
-            for (int i = lenth / 2 - 1; i >= 0; i--)
+            for (int i = lenth / 2 - 1; i >= 0; i--) // for begin we should look at last possible perent
             {
-                Hepify(arr, lenth, i);
+                Hepify(arr, lenth, i); // and we making hepify to look if it is bigger than left or right child 
             }
-            for (int i = lenth - 1; i > 0; i--)
+            for (int i = lenth - 1; i > 0; i--) // after that we need to check next value but i--
             {
-                Swap(ref arr[0], ref arr[i]);
-                Hepify(arr, i, 0);
+                Swap(ref arr[0], ref arr[i]); // at this point we making swap of the biggest value in array to the last position
+                Hepify(arr, i, 0); //and again hepify
             }
         }
         public void Swap(ref string a, ref string b)
         {
-            string temp = a;
+            string temp = a;//just swap function nothing special
             a = b;
             b = temp;
         }
         public void Hepify(string[] arr, int lenth, int root)
         {
-            int largest = root;
+            int largest = root; 
 
-            int left = 2 * root + 1;
-            int right = 2 * root + 2;
+            int left = 2 * root + 1;//check for left child
+            int right = 2 * root + 2;//check for right child
 
-            if (left < lenth && arr[left].CompareTo(arr[largest]) > 0) largest = left;
+            if (left < lenth && arr[left].CompareTo(arr[largest]) > 0) largest = left;// If left child is bigger, update largest
 
-            if (right < lenth && arr[right].CompareTo(arr[largest]) > 0) largest = right;
+            if (right < lenth && arr[right].CompareTo(arr[largest]) > 0) largest = right;// If right child is bigger, update largest
 
-            if (largest != root)
+            if (largest != root)// If largest is not root, swap and continue heapifying
             {
                 Swap(ref arr[root], ref arr[largest]);
                 Hepify(arr, lenth, largest);
